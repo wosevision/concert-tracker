d752edfbf33435cef9fc81b772a38bfc24bf9682
Wose <Jacksonteather@hotmail.com>
2017-08-24 21:52:34 -0400

    🛠 Basic app skeleton with addConcert

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <ul>
+      <li v-for="(concert, index) in concerts" :key="index">{{ concert.bands }}</li>
+    </ul>
+    <concert-form @addConcert="addConcert"></concert-form>
+  </div>
+</template>
+
+<script>
+import ConcertForm from './components/ConcertForm'
+
+export default {
+  name: 'app',
+  components: {
+    ConcertForm
+  },
+  computed: {
+    concerts () {
+      return this.$store.state.concerts
+    }
+  },
+  methods: {
+    addConcert ({ year, month, day, bands }) {
+      // const year = e.target.value
+      // console.log(e)
+      // if (year.trim()) {
+      this.$store.commit('addConcert', { year, month, day, bands })
+      // }
+      // e.target.value = ''
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ConcertForm.vue
@@ -0,0 +1,62 @@
+<template>
+  <form @submit.prevent="addConcert">
+    <label>
+      Year
+      <input type="number" value="2000" name="year" />
+    </label>
+    <label>
+      Month
+      <input type="number" value="9" name="month" />
+    </label>
+    <button type="submit">Add concert</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'concert-form',
+  // data () {
+  //   return {
+  //     msg: 'Welcome to Your Vue.js App'
+  //   }
+  // },
+  methods: {
+    addConcert (e) {
+      console.log(e)
+      const { year, month, day, bands } = e.target.elements
+      const elements = { year, month, day, bands }
+      // this.$emit('addConcert', { year, month, day, bands })
+      // const elements = e.target.elements
+      const concert = Object.keys(elements).reduce((acc, key) => {
+        const value = elements[key] ? elements[key].value : null
+        if (value) {
+          acc[key] = value
+        }
+        return acc
+      }, {})
+      this.$emit('addConcert', concert)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
