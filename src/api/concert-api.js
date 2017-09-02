bacb06462b9cc7604a3fdcdebe42feca9a97d83f
Wose <Jacksonteather@hotmail.com>
2017-09-01 22:35:29 -0400

    🐛 Fix fetching concerts from localStorage

--- src/api/concert-api.js
+++ src/api/concert-api.js
@@ -1,18 +1,17 @@
 /**
  * Mocking client-server processing
  */
-import _concerts from './concerts.json'
+import all from './concerts.json'
 
 // localStorage key token
 export const STORAGE_KEY = 'concert-tracker-storage'
 
 // for testing
 // window.localStorage.clear()
-
-const concerts = [...(JSON.parse(window.localStorage.getItem(STORAGE_KEY) || null) || _concerts)]
+const localStorage = (JSON.parse(window.localStorage.getItem(STORAGE_KEY) || null) || { all })
 
 export default {
   getConcerts (cb) {
-    setTimeout(() => cb(concerts), 100)
+    setTimeout(() => cb(localStorage.all), 100)
   }
 }
